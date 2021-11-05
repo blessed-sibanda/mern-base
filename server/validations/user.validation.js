82357ee64715365db1967cc05ed02cc295771e54
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-05 20:13:15 +0200

    refactor user validation

--- server/validations/user.validation.js
+++ server/validations/user.validation.js
@@ -2,21 +2,27 @@ const { body, check } = require('express-validator');
 
 import User from '../models/user.model';
 
+const uniqueEmail = (value) => {
+  return User.findOne({ email: value }).then((user) => {
+    if (user) return Promise.reject('E-mail already in use');
+  });
+};
+
 export const userCreationValidations = [
   check('name')
     .notEmpty()
     .withMessage('Name is required')
     .isLength({ min: 3 })
     .withMessage('Name must be at least 3 characters'),
-  check('email').isEmail().withMessage('Email is invalid'),
+
+  check('email')
+    .isEmail()
+    .withMessage('Valid email address is required')
+    .custom(uniqueEmail),
+
   check('password')
     .isLength({ min: 6 })
     .withMessage('Password must be at least 6 characters long'),
-  body('email').custom((value) => {
-    return User.findOne({ email: value }).then((user) => {
-      if (user) return Promise.reject('E-mail already in use');
-    });
-  }),
 ];
 
 export const userUpdateValidations = [
@@ -24,16 +30,15 @@ export const userUpdateValidations = [
     .optional()
     .isLength({ min: 3 })
     .withMessage('Name must be at least 3 characters'),
-  check('email').optional().isEmail().withMessage('Email is invalid'),
+
+  check('email')
+    .optional()
+    .isEmail()
+    .withMessage('Email is invalid')
+    .custom(uniqueEmail),
+
   check('password')
     .optional()
     .isLength({ min: 6 })
     .withMessage('Password must be at least 6 characters long'),
-  body('email')
-    .optional()
-    .custom((value) => {
-      return User.findOne({ email: value }).then((user) => {
-        if (user) return Promise.reject('E-mail already in use');
-      });
-    }),
 ];
