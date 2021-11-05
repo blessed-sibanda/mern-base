ef9bb8e47cfabdd38af3f11594e6c13fbfb59417
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-06 00:07:34 +0200

    baasic React setup

--- server/devBundle.js
+++ server/devBundle.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
-import config from '../webpack.config.server';
+import config from '../config';
 import webpackConfig from '../webpack.config.client';
 
 const compile = (app) => {
--- template.js
+++ template.js
@@ -7,10 +7,15 @@ export default () => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MERN Base</title>
+
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
   </head>
   <body>
-    <div id="root">Hello MERN</div>
-    <script src="/dist/bundle.js"></script>
+    <div id="root"></div>
+    <script type="text/javascript" src="/dist/bundle.js"></script>
   </body>
   </html>
   `;
